9352f0cc6723599c490a24fc70d2ad1f85cbfc67
obay.developer <obay.developer@gmail.com>
2024-08-30 12:56:39 +0000

    added theme toggel

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
-
+import ThemeSwitch from "./components/ThemeSwitch";
 export default function Home() {
   return (
     <>
+      <h1 className=" text-center text-3xl font-bold">
+        Welcome to falconMind
+      </h1>
+      <ThemeSwitch/>
     </>
   );
 }
